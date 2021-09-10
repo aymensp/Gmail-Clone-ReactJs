34b24d8ac9b61ce51e8ea38957e5500fef4fda6c
aymensp <aymen.smati@esprit.tn>
2021-09-10 21:29:33 +0100

    SideBar completed

--- src/Section.js
+++ src/Section.js
@@ -5,8 +5,7 @@ function Section({Icon , title, color,selected}) {
         <div className={`section ${selected && "section--selected"}`}
         style={{
             borderBottom:`3px solid ${color}`,
-            color: `${selected && color}`,
-            
+            color: `${selected && color}`, 
         }}
         >
             <Icon/>
